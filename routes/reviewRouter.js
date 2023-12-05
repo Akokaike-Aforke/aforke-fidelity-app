82851fb2c7be26fce86528cae383f75396b4e5a1
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-12-05 17:24:42 +0100

    added reviews

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -5,11 +5,20 @@ const userController = require("./../controllers/userController");
 const router = express.Router();
 
 //get the 5 best reviews
-router.route("/top-5-reviews").get(reviewController.aliasTopReviews, reviewController.getAllReviews)
-router.route("/review-stats").get(reviewController.getReviewStats)
-router.route("/searchReviews").get(reviewController.searchReviews)
-router.route("/updateReviewHelpful/:reviewID").patch(reviewController.updateReviewHelpful)
-router.route("/")
-.get(reviewController.getAllReviews)
-.post(authController.protect, userController.getMe, reviewController.createReview)
-module.exports = router
+router
+  .route("/top-5-reviews")
+  .get(reviewController.aliasTopReviews, reviewController.getAllReviews);
+router.route("/review-stats").get(reviewController.getReviewStats);
+router.route("/searchReviews").get(reviewController.searchReviews);
+router
+  .route("/updateReviewHelpful/:reviewID")
+  .patch(reviewController.updateReviewHelpful);
+router
+  .route("/")
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    userController.getMe,
+    reviewController.createReview
+  );
+module.exports = router;
